6afad53829d0df7bb6f66f43d0494eac9740abed
27qacem-crypto <27qacem@gmail.com>
2026-06-14 23:41:42 +0300

    Create main-script.js

---
+++ main-script.js
@@ -0,0 +1,4 @@
+const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbx1NfQxB4sv0M2_m8d02Td1lgTgXg/exec"; // تأكد إن ده الرابط الجديد بالظبط
+
+// تأكد إن الجزء اللي فيه fetch بيستخدم المتغير ده بس:
+// fetch(WEB_APP_URL, { ... })
